b6177eef18c7361ba6a815c787e4cb68a75e6040
Amandeep Saluja <amandeep.saluja21@gmail.com>
2020-12-26 03:17:29 +0530

    API Config: Decided to go with flickr

---
+++ WillyWeatherProject/DataLayer/APIs/APIConfiguration.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+
+struct APIConfiguration {
+    static let baseURL = "api.flickr.com"
+    static let timeout: TimeInterval = 60.0
+    static let apiKey = "062a6c0c49e4de1d78497d13a7dbb360"
+    static let perPage = 10
+}
