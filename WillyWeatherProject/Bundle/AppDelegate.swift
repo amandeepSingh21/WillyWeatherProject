6eef1cae6a0a9c2396df864df013c8e60f5a8be7
Amandeep Saluja <amandeep.saluja21@gmail.com>
2020-12-25 18:16:03 +0530

    Cleanup and folder re-structuring

---
+++ WillyWeatherProject/Bundle/AppDelegate.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let vc = MainViewController()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: vc)
+        return true
+    }
+
+  
+
+
+}
+
