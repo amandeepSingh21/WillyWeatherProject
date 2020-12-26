5176f90a44a112880fa98e463115259ed7c63ab6
Amandeep Saluja <amandeep.saluja21@gmail.com>
2020-12-26 21:51:55 +0530

    API: Change from Flickr to OMDB due to more data for Detail screen

--- WillyWeatherProject/DataLayer/APIs/APIConfiguration.swift
+++ WillyWeatherProject/DataLayer/APIs/APIConfiguration.swift
@@ -2,8 +2,8 @@ import Foundation
 
 
 struct APIConfiguration {
-    static let baseURL = "api.flickr.com"
+    static let baseURL = "omdbapi.com"
     static let timeout: TimeInterval = 60.0
-    static let apiKey = "062a6c0c49e4de1d78497d13a7dbb360"
+    static let apiKey = "7166f140"
     static let perPage = 10
 }
