6d3d5253e80bfd7a603ba8d18578fd742b7c1976
Amandeep Saluja <amandeep.saluja21@gmail.com>
2020-12-26 21:56:35 +0530

    VC: Modify API calling

--- WillyWeatherProject/UILayer/ViewControllers/MainViewController.swift
+++ WillyWeatherProject/UILayer/ViewControllers/MainViewController.swift
@@ -5,13 +5,14 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .red
-        let api = FlickrPhotosSearchAPI()
+        let api = OMDBFetchMoviesAPI()
         var r = HTTPRequest()
-        r.path = "/services/rest/"
-        r.setQueryParams(params: FlickrRequest(text: "bat", page: 1))
-        api.query(request: r ) { (res) in
+        
+        r.setQueryParams(params: OMDBRequest(text: "batman", page: 1))
+        api.fetch(request: r) { (res) in
             
         }
+       
     }
 
 
