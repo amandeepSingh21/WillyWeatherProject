c8c407a2609bfa392c67eaf2932fc15dcb06de54
Amandeep Saluja <amandeep.saluja21@gmail.com>
2020-12-27 09:29:16 +0530

    UI:  Coordinate and trigger key events in main view controller

--- WillyWeatherProject/Bundle/AppDelegate.swift
+++ WillyWeatherProject/Bundle/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let vc = MainViewController()
+        let vc = MainViewController(viewModel: OMDBViewModel(repository: OMDBFetchMoviesAPI()))
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.rootViewController = UINavigationController(rootViewController: vc)
--- WillyWeatherProject/UILayer/ViewControllers/MainViewController.swift
+++ WillyWeatherProject/UILayer/ViewControllers/MainViewController.swift
@@ -1,20 +1,70 @@
 import UIKit
 
-class MainViewController: UIViewController {
 
+class MainViewController: NiblessViewController {
+    
+    //MARK: - Child View Controller
+    let intermediateViewController: IntermediateViewController
+    
+    //MARK: - Properties
+    var rootView: OMDBView
+    let viewModel: OMDBViewModel
+    
+    //MARK: - Methods
+    init(viewModel:OMDBViewModel) {
+        self.viewModel = viewModel
+        self.intermediateViewController = IntermediateViewController(message: viewModel.viewState.value.message)
+        
+        rootView = OMDBView(viewModel: viewModel, frame: .zero)
+        super.init()
+        rootView.delegate = self
+        
+        
+        
+    }
+    
+    
+    override func loadView() {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .white
+        self.view = rootView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
-        let api = OMDBFetchMoviesAPI()
-        var r = HTTPRequest()
+        self.title = "OMDB"
+        viewModel.fetch()
+        setUpBindings()
+      
         
-        r.setQueryParams(params: OMDBRequest(text: "batman", page: 1))
-        api.fetch(request: r) { (res) in
-            
-        }
-       
     }
-
-
+    
+ 
+    private func setUpBindings() {
+        self.viewModel.viewState.bind { [weak self] state in
+            guard let self = self else { return }
+            self.intermediateViewController.message = state.message
+            switch state {
+            case .loading,.noResultsFound:
+                self.addFullScreen(childViewController: self.intermediateViewController)
+            case .error(let error):
+                self.remove(childViewController: self.intermediateViewController)
+                self.present(errorMessage: error.message)
+            case .loaded:
+                self.remove(childViewController: self.intermediateViewController)
+                
+            }
+        }
+        
+    }
+    
+    
 }
 
+extension MainViewController: OMDBViewViewSelectionDelegate {
+    func didSelectItem(item: Movie) {
+        
+    }
+    
+    
+}
